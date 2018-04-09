baa7ccbbd16853bfb1de54c6ba87e17b9ddc1688
Arto Santala <arto.santala@solita.fi>
2018-04-09 12:49:21 +0300

    Initial import, just what initializr created

---
+++ src/main/java/fi/solita/java10demo/Java10demoApplication.java
@@ -0,0 +1,12 @@
+package fi.solita.java10demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java10demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java10demoApplication.class, args);
+	}
+}
---
+++ src/test/java/fi/solita/java10demo/Java10demoApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.solita.java10demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java10demoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
